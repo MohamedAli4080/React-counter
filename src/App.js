d16a5d32d71d2da44eb2b4a05b85ef445d805f54
MAJORELKSA\75952 <m.gomaa@majorel.com>
2023-12-10 10:43:35 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [timer,setTimer]=useState(0);
+
+  const [count,setCount]=useState(0);
+
+  const startTimer=()=>{
+    setTimer(10);
+    setCount(0);
+    
+  }
+  useEffect(() => {
+    if (timer===0) return;
+
+      var interval=setInterval(() => {
+          setTimer(c=>c-1);
+      }, 1000);
+
+    
+      return () => {
+        console.log('destroy');
+        clearInterval(interval);
+      };
+  }, [timer])
+  
+  const click=()=>{
+    if (timer>0)
+      setCount(c=>c+1);
+  }
+  const reset=()=>{
+    setTimer(10);
+    setCount(0);
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Timer :{timer}</h2>
+        <h2>Count :{count}</h2>
+        <button onClick={startTimer}>start</button>
+        <button onClick={click}>Click</button>
+        <button onClick={reset}>Reset</button>
       </header>
     </div>
   );
